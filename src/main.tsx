c554d41fec10b8d3763d22be7634007e741f4e83
felipe-silveira-dev <felipe.silveira@eduzz.com.br>
2023-05-30 09:26:35 -0300

    adding error page

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { 
   createBrowserRouter,
   RouterProvider
- } from 'react-router-dom'
-import './index.css'
-import Root from './routes/root.jsx'
+ } from 'react-router-dom';
+import './index.css';
+import Root from './routes/root';
+import ErrorPage from './error-page'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Root />
+    element: <Root />,
+    errorElement: <ErrorPage />
   }
 ]);
 
