6828ac5f411eed950f13403a948fb5efdf2c792f
felipe-silveira-dev <felipe.silveira@eduzz.com.br>
2023-05-30 11:07:19 -0300

    edit page with action and redirect

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { 
-  createBrowserRouter,
-  RouterProvider
- } from 'react-router-dom';
 import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Root, { loader as rootLoader, action as rootAction } from './routes/root';
 import ErrorPage from './error-page'
 import Contact, { loader as contactLoader } from './routes/contact'
+import Edit, { action as actionEdit } from './routes/edit';
 
 const router = createBrowserRouter([
   {
@@ -21,6 +19,12 @@ const router = createBrowserRouter([
         path: 'contacts/:contactId',
         element: <Contact />,
         loader: contactLoader,
+      },
+      {
+        path: 'contacts/:contactId/edit',
+        element: <Edit />,
+        loader: contactLoader,
+        action: actionEdit,
       }
     ]
   },
